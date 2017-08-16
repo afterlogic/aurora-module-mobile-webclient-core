9a4f88ae9eae1f2b10255ad9f4dbaf7ed15e637a
sash04ek <a.ovcharov@gmail.com>
2017-08-16 13:54:18 +0300

    licensing support is added

---
+++ Module.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @copyright Copyright (c) 2017, Afterlogic Corp.
+ * @license AfterLogic Software License
+ *
+ * This code is licensed under AfterLogic Software License.
+ * For full statements of the license see LICENSE files.
+ */
+
+namespace Aurora\Modules\ContactsMobileWebclient;
+
+/**
+ * @package Modules
+ */
+class Module extends \Aurora\System\Module\AbstractLicensedModule { }
