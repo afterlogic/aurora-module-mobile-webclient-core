7c91da63a6100278fe6023449cf15a53f64f83e3
TimofeevVadim <vtimofeev223@gmail.com>
2022-02-19 13:08:03 +0300

    Added AppDialog and CancelCrossIcon components

---
+++ vue-mobile/src/components/common/AppDialog.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-dialog v-bind="$attrs" @escape-key="cancelDialog">
+    <q-card class="q-dialog-size dialog" style="min-width: 300px">
+      <div class="dialog__head">
+        <slot name="head"/>
+      </div>
+      <q-card-actions align="right">
+        <slot name="actions" />
+      </q-card-actions>
+      <cancel-cross-icon v-if="showCross" class="cancel-icon" @click="cancelDialog"/>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script>
+
+import CancelCrossIcon from "components/common/icons/CancelCrossIcon";
+
+export default {
+  name: "AppDialog",
+  components: {
+    CancelCrossIcon
+  },
+  props: {
+    cancelDialog: { type: Function, default: () => null },
+    showCross: { type: Boolean, default: () => true }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.cancel-icon {
+  position: absolute;
+  top: 16px;
+  right: 16px
+}
+.dialog {
+  &__head {
+    padding-top: 36px
+  }
+}
+</style>
---
+++ vue-mobile/src/components/common/icons/CancelCrossIcon.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect width="16" height="16" fill="white"/>
+    <path d="M2 2L14 14" stroke="#469CF8" stroke-width="2"/>
+    <path d="M14 2L2 14" stroke="#469CF8" stroke-width="2"/>
+  </svg>
+
+</template>
+
+<script>
+export default {
+  name: "CancelCrossIcon"
+}
+</script>
+
+<style scoped>
+
+</style>
