4c9e7e08642fded93b2abdc2f628cadc2e5314e0
TimofeevVadim <vtimofeev223@gmail.com>
2022-03-16 16:20:48 +0300

    Improvement code

--- vue-mobile/src/utils/contacts/utils.js
+++ vue-mobile/src/utils/contacts/utils.js
@@ -1,15 +1,28 @@
+import store from 'src/store'
+
 export const getContactsSelectOptions = (contacts, selectContacts) => {
   const filteredContacts = []
+  const currentUserEmail = store.getters['core/userPublicId']
   contacts.forEach((contact) => {
-    const index = selectContacts.findIndex( selectContact => selectContact.email === contact.ViewEmail)
-      if (index === -1) {
+    const index = selectContacts.findIndex( selectContact => selectContact.email === (contact.ViewEmail || contact.email))
+      if (index === -1 && currentUserEmail !== (contact.ViewEmail || contact.email)) {
         filteredContacts.push({
-          email: contact.ViewEmail,
-          label: contact.ViewEmail,
-          value: contact.ViewEmail,
+          email: contact.ViewEmail || contact.email,
+          label: contact.ViewEmail || contact.email,
+          value: contact.ViewEmail || contact.email,
           status: '',
         })
       }
   })
   return filteredContacts
+}
+export const getAllContactsSelectOptions = (contacts) => {
+  return contacts.map( contact => {
+    return {
+      email: contact.ViewEmail || contact.email,
+      label: contact.ViewEmail || contact.email,
+      value: contact.ViewEmail || contact.email,
+      status: '',
+    }
+  } )
 }
