47f232e4b7ca56548d4b3fdb4f3f74fbc8c86abc
Vasil Sokolov <vasil@afterlogic.com>
2022-10-07 18:39:38 +0200

    refactored AppCreateButton definition and usage

--- vue-mobile/src/components/common/AppCreateButton.vue
+++ vue-mobile/src/components/common/AppCreateButton.vue
@@ -1,36 +1,49 @@
 <template>
-  <plus-btn-icon @click="showDialog" :class="`create-btn ${classes}`" />
+  <!-- <div class="app-create-button" @click="showDialog" :class="{'rotate': rotate}"> -->
+  <div class="app-create-button" :class="{'rotate': rotate}">
+    <slot>
+      <plus-icon color="#fff" />
+    </slot>
+  </div>
 </template>
 
 <script>
-import PlusBtnIcon from 'components/common/icons/PlusBtnIcon'
+// import PlusBtnIcon from 'components/common/icons/PlusBtnIcon'
+import PlusIcon from 'components/common/icons/Plus1Icon'
 
 export default {
   name: 'AppCreateButton',
   components: {
-    PlusBtnIcon,
+    PlusIcon,
   },
   props: {
     classes: { type: String, default: 'z-index-min' },
-    showDialog: { type: Function, default: () => null }
+    // showDialog: { type: Function, default: () => null },
+    rotate: { type: Boolean, default: false },
   },
 }
 </script>
 
-<style scoped>
-.create-btn {
+<style lang="scss" scoped>
+  
+.app-create-button {
   position: fixed;
-  bottom: 68px;
-  right: 10px;
+  bottom: 76px;
+  right: 16px;
   transition-duration: 300ms;
+  background-color: #469CF8;
+  box-shadow: 0 0 10px #469CF8;
+  border-radius: 100px;
+  width: 56px;
+  height: 56px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 1;
 }
 .rotate {
   transform: rotate(225deg);
-}
-.z-index-max {
   z-index: 10000;
-}
-.z-index-min {
-  z-index: 1;
+  box-shadow: 0 0 14px darken(#469CF8, 10%);
 }
 </style>
---
+++ vue-mobile/src/components/common/icons/Plus1Icon.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M16 8H0" :stroke="color" stroke-width="2"/>
+    <path d="M8 0L8 16" :stroke="color" stroke-width="2"/>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'PlusIcon',
+  props: {
+    color: { type: String, default: '#000' }
+  },
+}
+</script>
+
+<style scoped></style>
