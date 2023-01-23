f45cdc07fdc23e4f8b6e439b48c87a8c63eab105
Vasil Sokolov <vasil@afterlogic.com>
2023-01-23 23:21:46 +0200

    Updated dates in copyright

--- Module.php
+++ Module.php
@@ -10,7 +10,7 @@ namespace Aurora\Modules\CoreMobileWebclient;
  * Mobile webclient for core view models.
  *
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
- * @copyright Copyright (c) 2019, Afterlogic Corp.
+ * @copyright Copyright (c) 2023, Afterlogic Corp.
  *
  * @package Modules
  */
