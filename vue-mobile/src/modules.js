5866434320ffaa5e938afd29d7b81982c7cf687b
Vasil Sokolov <vasil@afterlogic.com>
2024-02-12 11:06:11 +0200

    Uncommented modules in auto generated modules.js file

--- vue-mobile/src/modules.js
+++ vue-mobile/src/modules.js
@@ -3,10 +3,10 @@ export default {
     return [
       await import('src/manager'),
       await import('src/../../../ContactsMobileWebclient/vue-mobile/manager'),
-      // await import('src/../../../CoreParanoidEncryptionWebclientPlugin/vue-mobile/manager'),
+      await import('src/../../../CoreParanoidEncryptionWebclientPlugin/vue-mobile/manager'),
       await import('src/../../../FilesMobileWebclient/vue-mobile/manager'),
       await import('src/../../../MailMobileWebclient/vue-mobile/manager'),
-      // await import('src/../../../OpenPgpFilesMobileWebclient/vue-mobile/manager'),
+      await import('src/../../../OpenPgpFilesMobileWebclient/vue-mobile/manager'),
       await import('src/../../../OpenPgpMobileWebclient/vue-mobile/manager'),
       await import('src/../../../SettingsMobileWebclient/vue-mobile/manager'),
       await import('src/../../../StandardLoginFormMobileWebclient/vue-mobile/manager'),
