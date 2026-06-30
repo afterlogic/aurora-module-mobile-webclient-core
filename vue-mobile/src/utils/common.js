11c32bf0a532b597fa0061086316d7307e8d15d1
V.Asadov <vladislav.asadov@dev.afterlogic.com>
2026-06-30 14:41:57 +0300

    Fix default page after login to open mail instead of contacts.
    pickHighestOrder used item.name while pages expose pageName, so the mail tab was never selected.

--- vue-mobile/src/utils/common.js
+++ vue-mobile/src/utils/common.js
@@ -1,19 +1,10 @@
 export const pickHighestOrder = (objects, order) => {
-  // Initialize a variable to store the lowest index and the corresponding object
-  let lowestIndex = 0
-  let mostPrefferedRoute = null
-
-  // Loop through the people array
-  for (let item of objects) {
-    // Get the index of the person's name in the modulesOrder array
-    let index = order.indexOf(item.name)
-
-    // If the index is lower than the current lowest index, update the lowest index and the highest order object
-    if (index < lowestIndex) {
-      lowestIndex = index
-      mostPrefferedRoute = item
+  for (const name of order) {
+    const item = objects.find((obj) => (obj.pageName ?? obj.name) === name)
+    if (item) {
+      return item
     }
   }
 
-  return mostPrefferedRoute
+  return objects[0] ?? null
 }
